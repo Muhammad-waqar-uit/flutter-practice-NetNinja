56e2d47687c5af1466f96054d75a9fd855900017
Muhammad-waqar-uit <muwaqar@students.uit.edu>
2022-09-19 01:46:30 +0500

    network image

--- netninja/lib/main.dart
+++ netninja/lib/main.dart
@@ -19,15 +19,9 @@ class test extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 99, 197, 120),
       ),
       body: const Center(
-        child: Text(
-          'Hello Ninjas',
-          style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.amber,
-              fontFamily: 'BungeeSpice'),
-        ),
-      ),
+          child: Image(
+              image: NetworkImage(
+                  'https://static.remove.bg/remove-bg-web/c4b29bf4b97131238fda6316e24c9b3606c18000/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg'))),
       // ignore: prefer_const_constructors
       floatingActionButton: FloatingActionButton(
         onPressed: () {
